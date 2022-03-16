483073efb65d273411c930d5343368bb5c8264ec
ujixTW <99461284+ujixTW@users.noreply.github.com>
2022-03-16 11:56:13 +0800

    新增考試題目的Model 至 Models資料夾

---
+++ Sakei/Models/TestDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sakei.Models
+{
+    public class TestDataModel
+    {
+        public Guid TestID { get; set; }
+        public byte TestLevel { get; set; }
+        public string TestContent { get; set; }
+        public char TestAnswer { get; set; }
+        public bool IsEnable { get; set; }
+    }
+}
