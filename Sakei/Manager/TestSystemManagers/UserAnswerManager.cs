7cf71b2bd7e2649cf7729b0d95eca155cb666cca
ujixTW <99461284+ujixTW@users.noreply.github.com>
2022-03-20 22:36:41 +0800

    新增 TestSystemManagers 於 Managers底下。 新增用於留言板、積分、題庫資料、作答紀錄的 Manager 於 TestSystemManagers

---
+++ Sakei/Manager/TestSystemManagers/UserAnswerManager.cs
@@ -0,0 +1,20 @@
+﻿using Sakei.Models.TestSystemModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sakei.Manager.TestSystemManagers
+{
+    public class UserAnswerManager
+    {
+        //查詢
+        public List<UserAnswerModel> GetUserAnswerList()
+        {
+
+            return new List<UserAnswerModel>();
+        }
+        //增加
+        //修改
+    }
+}
