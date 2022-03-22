3caba1a11c41f4a6b2c0d4766270a7d7149384cd
ujixTW <99461284+ujixTW@users.noreply.github.com>
2022-03-22 12:59:42 +0800

    新增 ChoseTestLevelHelper於新建於Helper資料夾下的TestSystemHelpers中

---
+++ Sakei/Helper/TestSystemHelpers/ChoseTestLevelHelper.cs
@@ -0,0 +1,26 @@
+﻿using Sakei.Manager.TestSystemManagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sakei.Helper.TestSystemHelpers
+{
+    public class ChoseTestLevelHelper
+    {
+        /// <summary>
+        /// 判斷使用者等級是否足夠
+        /// </summary>
+        /// <param name="userLevel">使用者等級</param>
+        /// <param name="targetLevel">測試、物品等級</param>
+        /// <returns></returns>
+        public static bool IsLevelEnough(int userLevel, int targetLevel)
+        {
+            if (userLevel > targetLevel)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
