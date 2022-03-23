dc5b4540c2eff8d52f3fdbd539a8efed704dd3e8
ujixTW <99461284+ujixTW@users.noreply.github.com>
2022-03-23 22:01:13 +0800

    重新命名 TestReviewPage 改為 TestReviewList

--- Sakei/TestSystem/TestReviewList.aspx.cs
+++ Sakei/TestSystem/TestReviewList.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Sakei.TestSystem
 {
-    public partial class TestReviewPage : System.Web.UI.Page
+    public partial class TestReviewList : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
