2f5854af33bf9b623c4867ba34127a4ec5f69ec7
HansCHang83 <89730426+HansChang83@users.noreply.github.com>
2022-03-26 19:34:30 +0800

    變更新增商品的HTML

---
+++ Sakei/EditItem.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FrontMaster.Master" AutoEventWireup="true"  CodeBehind="EditItem.aspx.cs" Inherits="Sakei.EditItem" %>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <label  class="label_title">新增題目</label><br />
+        <label class="label_input">帳號</label>
+    <asp:TextBox runat="server"    CssClass="text_field" ID="txtAcc"  placeholder="【帳號】輸入８～２０字元" ></asp:TextBox><br />
+
+      <label class="label_input">密碼</label>
+    <asp:TextBox runat="server" CssClass="text_field" ID="txtPWD"  placeholder="【密碼】輸入８～２０字元" ></asp:TextBox><br />
+      <label class="label_input">使用者姓名 </label>
+    <asp:TextBox runat="server" CssClass="text_field" ID="txtName"  placeholder="【使用者姓名】" ></asp:TextBox><br />
+     <label class="label_input">信箱 </label>
+    <asp:TextBox runat="server" CssClass="text_field" ID="txtMail"  placeholder="【信箱】" ></asp:TextBox><br />
+        
+    <label class="label_input">題目等級 </label>
+        <asp:DropDownList ID="intLevel" runat="server" AutoPostBack="True">
+            <asp:ListItem Value="1"></asp:ListItem>
+            <asp:ListItem Value="2">N2</asp:ListItem>
+            <asp:ListItem Value="3">N3</asp:ListItem>
+            <asp:ListItem Value="4">N4</asp:ListItem>
+            <asp:ListItem Value="5">N5</asp:ListItem>
+    </asp:DropDownList><br />
+
+
+    
+        <asp:Literal runat="server" ID="ltlmsg"></asp:Literal>
+        <asp:Button runat="server" CssClass="wweebtn" ID="btnConfirm" Text="確定" OnClick="btnConfirm_Click" />
+
+
+      
+    <asp:Button runat="server" CssClass="wweebtn" ID="btnCancel" Text="取消重填" OnClick="btnCancel_Click" />
+    <asp:Literal ID="ltl1" runat="server"></asp:Literal>
+   
+    
+
+</asp:Content>
---
+++ Sakei/EditItem.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <自動產生的>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生的>
+//------------------------------------------------------------------------------
+
+namespace Sakei
+{
+
+
+    public partial class EditItem
+    {
+
+        /// <summary>
+        /// txtAcc 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAcc;
+
+        /// <summary>
+        /// txtPWD 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPWD;
+
+        /// <summary>
+        /// txtName 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+
+        /// <summary>
+        /// txtMail 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMail;
+
+        /// <summary>
+        /// intLevel 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList intLevel;
+
+        /// <summary>
+        /// ltlmsg 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltlmsg;
+
+        /// <summary>
+        /// btnConfirm 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnConfirm;
+
+        /// <summary>
+        /// btnCancel 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancel;
+
+        /// <summary>
+        /// ltl1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltl1;
+    }
+}
